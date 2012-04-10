bc72611205d9a0c5850f8cc7402a1a9db4611b5b
imvkmark <zhaody901@126.com>
2012-04-10 22:47:34 +0800

    移动mail文件夹和删除静态页面

--- mail/admin/admin.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-defined('IN_WSLM') or exit('Access Denied');
-require_once WSLM_ROOT.'languages/zh-cn/mail_admin.lang.php';
-define('MOD_ROOT', WSLM_ROOT.''.$mod.'/');
-?>
--- mail/uninstall/mysql.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE IF EXISTS `wslm_mail`;
-DROP TABLE IF EXISTS `wslm_mail_email`;
-DROP TABLE IF EXISTS `wslm_mail_email_type`;
